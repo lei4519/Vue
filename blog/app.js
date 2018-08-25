e024078ba576961b00fd7923ecd2ab9a742b95b3
lei4519 <939690017@qq.com>
2018-08-25 20:57:20 +0800

    "静态网页部署完毕"

--- blog/app.js
+++ blog/app.js
@@ -6,8 +6,11 @@ const app = express()
 app.use('/public', express.static(path.join(__dirname, './public/')))
 app.use('/node_modules', express.static(path.join(__dirname, './node_modules/')))
 
+app.engine('html', require('express-art-template'))
+app.set('views', path.join(__dirname, './views/'))
+
 app.get('/', function (req, res) {
-	res.send('hello')
+	res.render('index.html')
 })
 
 app.listen(3000, function () {
