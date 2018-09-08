6de7f106ada6f10868b87e4ae98b0ff2d5785929
lei4519 <939690017@qq.com>
2018-09-08 23:58:15 +0800

    babel

--- webp/webpack-BasicUse/src/index.js
+++ webp/webpack-BasicUse/src/index.js
@@ -1,11 +1,26 @@
 import $ from 'jquery'
 
+// 导入css样式表
+import './css/index.css'
+
+import './css/index.less'
+
+import './css/index.scss'
+
 $(function () {
   $('li:odd').css('background-color', 'cyan')
   $('li:even').css('background-color', function () {
       return '#' + 'f00012'
   })
 })
+
+class ClassWithStaticMethod {
+  static staticMethod() {
+    return '123123123';
+  }
+}
+
+console.log(ClassWithStaticMethod.staticMethod())
 
 /*
 * webpack能够处理js文件之间的相互依赖关系
--- webp/webpack-BasicUse/webpack.config.js
+++ webp/webpack-BasicUse/webpack.config.js
@@ -1,6 +1,9 @@
 const path = require('path')
 // 启用热加载的第二步
 const webpack = require('webpack')
+// 插件的两个作用
+// 1 自动在内存中根据指定页面生成一个内存中的页面
+// 2 自动把打包好的bundle.js追加到页面中去
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
@@ -27,5 +30,16 @@ module.exports = {
 			template: path.resolve(__dirname, './src/index.html'),
 			filename: 'index.html'
 		})
-	]
+	],
+  module: { // 配置所有的第三方模块加载器
+    rules: [ // 所有第三方模块的匹配规则
+      {test: /\.css$/, use: ['style-loader', 'css-loader']},
+      {test: /\.less$/, use: ['style-loader', 'css-loader', 'less-loader']},
+      {test: /\.scss$/, use: ['style-loader', 'css-loader', 'sass-loader']},
+			// limit 给定的值是图片的大小 单位是byte 如果我们引用的图片大于等于给定的值 则不会转换为base64位格式的字符串
+      {test: /\.(jpg|png|gif|bmp)$/, use: 'url-loader?limit=1024'},
+      {test: /\.(ttf|eot|svg|woff|woff2)$/, use: 'url-loader'},
+			{test: /\.js$/, exclude: /node_modules/, use: 'babel-loader'}
+		]
+  }
 }
